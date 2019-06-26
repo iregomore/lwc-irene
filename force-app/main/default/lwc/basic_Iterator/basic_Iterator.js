0133fb287b4ea222af2d138d518d4d50b2e0a380
iregomore <iregomore@gmail.com>
2019-06-26 12:27:37 +0200

    added for iterator instead for each

---
+++ force-app/main/default/lwc/basic_Iterator/basic_Iterator.js
@@ -0,0 +1,19 @@
+import { LightningElement } from 'lwc';
+
+export default class Basic_Iterator extends LightningElement {
+
+    contacts = [
+        {
+            name : "Irene",
+            email : "iregomore@gmail.com"
+        },
+        {
+            name : "Irene2",
+            email : "iregomore2@gmail.com"
+        },
+        {
+            name : "Irene3",
+            email : "iregomore3@gmail.com"
+        }
+    ]
+}
