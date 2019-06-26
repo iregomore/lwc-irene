20ab31f41eaad130e8d149993ccd92dee0b922b3
iregomore <iregomore@gmail.com>
2019-06-26 11:59:26 +0200

    for each

---
+++ force-app/main/default/lwc/basic_ForEach/basic_ForEach.js
@@ -0,0 +1,14 @@
+import { LightningElement } from 'lwc';
+
+export default class Basic_ForEach extends LightningElement {
+    contacts = [
+        {
+            name : "Irene",
+            email : "iregomore@gmail.com"
+        },
+        {
+            name : "Irene2",
+            email : "iregomore2@gmail.com"
+        }
+    ]
+}
