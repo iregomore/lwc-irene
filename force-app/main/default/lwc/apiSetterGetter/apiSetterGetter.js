3cd43d08eed3f8607abde24ab3954476dbb76657
iregomore <iregomore@gmail.com>
2019-06-26 15:54:32 +0200

    added parent component to set properties to child

---
+++ force-app/main/default/lwc/apiSetterGetter/apiSetterGetter.js
@@ -0,0 +1,10 @@
+import { LightningElement, track } from 'lwc';
+
+export default class ApiSetterGetter extends LightningElement {
+    @track description
+    @track priority
+
+    handleClick() {
+        
+    }
+}
